09788c41f212ccf4b887856a22cf380a4d10b697
George MacKerron <georgemackerron@neon.tech>
2026-03-04 17:46:36 +0000

    Progressive output for HTML conversion script

--- logs-to-html.rb
+++ logs-to-html.rb
@@ -1,27 +1,26 @@
 #!/usr/bin/env ruby
 # elephantshark --no-bw | ./logs-to-html.rb > ~/Downloads/eslog.html
 
-html = %{<!DOCTYPE html>
+STDOUT.sync = true
+
+puts %{<!DOCTYPE html>
 <html><head><title>elephantshark log</title>
   <style>
     body { margin: 0; padding: 0; overflow-x: scroll; background: #002050; color: #eee; }
     pre { display: inline-block; padding: 10px 20px; margin: 0; font: 14px/1.4 'IBM Plex Mono', monospace; }
     .c33 { color: #ffee00; }
+    .c34 { color: #0044ff; }
     .c35 { color: #00dddd; }
     .c36 { color: #ff44ff; }
   </style>
-</head><body><pre>
-}
+</head><body><pre>}
 
-ARGF.each do |raw|
-  line = raw.chomp
-    .gsub(/[<>"']/, { '<' => '&lt;', '>' => '&gt;', '"' => '&quot;', "'" => '&apos;' })
-    .gsub(/\033\[(\d+)m(.*?)\033\[0m/, '<span class="c\1">\2</span>')
-  html += line + "\n"
+begin
+  ARGF.each do |raw|
+    puts raw.chomp
+      .gsub(/[<>"']/, { '<' => '&lt;', '>' => '&gt;', '"' => '&quot;', "'" => '&apos;' })
+      .gsub(/\033\[(\d+)m(.*?)\033\[0m/, '<span class="c\1">\2</span>')
+  end
+ensure
+  puts %{</pre></body></html>}
 end
-
-html += %{
-</pre></body></html>
-}
-
-puts html
